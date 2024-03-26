db7bf3dd04feb3c4d67fa4b9d076bf60797b4693
Miriam <miriamvoelkl@gmail.com>
2024-03-26 16:20:06 +0100

    add basic pattern card

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,29 @@
+import initialSewingPatterns from "../lib/initialSewingPatterns.json";
+import styled from "styled-components";
+
 export default function HomePage() {
   return (
-    <div>
+    <>
       <h1>Sewing Pattern Organizer</h1>
-    </div>
+      <ul>
+        {initialSewingPatterns.map((pattern) => (
+          <li key={pattern.id}>
+            <StyledCard>
+              <h2>{pattern.name}</h2>
+              <h3>{pattern.designer}</h3>
+              <p>Category: {pattern.category}</p>
+              <section>
+                <p>Yardage: {pattern.fabricYardage}</p>
+                <p>Notions: {pattern.notions}</p>
+              </section>
+            </StyledCard>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
+
+const StyledCard = styled.div`
+  background-color: goldenrod;
+`;
