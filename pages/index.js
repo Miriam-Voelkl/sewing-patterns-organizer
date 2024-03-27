b9432d3cf8c6a3ad2b8349f7750f5010bc925992
Miriam <miriamvoelkl@gmail.com>
2024-03-27 14:34:31 +0100

    add handleSubmit function and local storage

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,28 @@
 import styled from "styled-components";
 import Link from "next/link";
+import useLocalStorageState from "use-local-storage-state";
+import { uid } from "uid";
 
 export default function HomePage({ initialSewingPatterns }) {
+  const [sewingPatterns, setSewingPatterns] = useLocalStorageState("patterns", {
+    defaultValue: initialSewingPatterns,
+  });
+
+  function handlePatternSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    setSewingPatterns([...sewingPatterns, { ...data, id: uid() }]);
+
+    event.target.reset();
+  }
+
   return (
     <>
       <h1>Sewing Pattern Organizer</h1>
       <StyledList>
-        {initialSewingPatterns.map((pattern) => (
+        {sewingPatterns.map((pattern) => (
           <li key={pattern.id}>
             <StyledCard>
               <h2>{pattern.name}</h2>
@@ -18,13 +34,13 @@ export default function HomePage({ initialSewingPatterns }) {
         ))}
       </StyledList>
       <h2>add a new pattern:</h2>
-      <form>
+      <form onSubmit={handlePatternSubmit}>
         <label htmlFor="pattern-name">Name</label>
-        <input type="text" id="pattern-name" name="patternName"></input>
+        <input type="text" id="pattern-name" name="name"></input>
         <label htmlFor="pattern-designer">Designer</label>
-        <input type="text" id="pattern-designer" name="patternDesigner"></input>
+        <input type="text" id="pattern-designer" name="designer"></input>
         <label htmlFor="pattern-category">Category</label>
-        <select id="pattern-category" name="patternCategory">
+        <select id="pattern-category" name="category">
           <option value="tops">tops</option>
           <option value="jackets-coats">jackets & coats</option>
           <option value="cardigans-waistcoats">cardigans & waistcoats</option>
@@ -37,14 +53,10 @@ export default function HomePage({ initialSewingPatterns }) {
         <input
           type="textarea"
           id="pattern-yardage"
-          name="patternYardage"
+          name="fabricYardage"
         ></input>
         <label htmlFor="pattern-notions">Required Notions</label>
-        <input
-          type="textarea"
-          id="pattern-notions"
-          name="patternNotions"
-        ></input>
+        <input type="textarea" id="pattern-notions" name="notions"></input>
         <button type="submit">Submit</button>
       </form>
     </>
